205309a6bc839dbb1b7eca1c70a370beaa9fa41c
pkarasek <pawel.j.karasek@gmail.com>
2018-04-14 03:10:46 +0200

    cx4

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,32 @@
 from django import forms
 from .models import Post
+from django.contrib.auth import authenticate, get_user_model, login, logout
+
+User = get_user_model()
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+"""
+class UserLoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget = forms.PasswordInput)
+
+    def clean(self):
+        username = self.cleaned_data.get("username")
+        password = self.cleaned_data.get("password")
+
+        if username and password:
+            user = authenticate(username = username, password = password)
+            if not user:
+                raise forms.ValidationError("Uzytkownik nie istnieje")
+
+            if not user.check_password(password):
+                raise forms.ValidationError("Niepoprawne haslo")
+
+            if not user.is_active:
+                raise forms.ValidationError("Konto zablokowane")
+        return super(UserLoginForm, self).clean()
+        """
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,32 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth import authenticate, get_user_model, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+
+def login_view(request):
+    title = "LOGOWANIE"
+    if request.method == 'POST':
+        form = AuthenticationForm(data = request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('post_list')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'blog/login_view.html', {"form":form, "title":title})
+
+def logout_view(request):
+
+    logout(request)
+    return redirect('post_list')
+
+def register_view(request):
+    return render(request, 'blog/register_view.html', {})
+
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
@@ -12,6 +38,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk = pk)
     return render(request, 'blog/post_detail.html', {'post':post})
 
+@login_required(login_url = 'login_view')
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -25,6 +52,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form':form})
 
+@login_required(login_url = 'login_view')
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk = pk)
     if request.method == "POST":
